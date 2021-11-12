37080e1b715fb6bcc75826816a1e36fdb6a823bc
unknown <mahi121.mr@gmail.com>
2021-11-13 01:16:17 +0600

    banner added

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,14 +1,37 @@
 import React from 'react';
+import background from '../../../images/banner.png'
+import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
+import { Button, Typography } from '@mui/material';
+
+const bannerBg = {
+    background: `url(${background})`,
+    marginTop: '90px',
+    height: '100vh',
+    backgroundRepeat: 'no-repeat',
+    // backgroundAttachment: 'fixed',
+    backgrounPosition: 'center',
+    backgroundSize: 'cover',
+}
 
 const Banner = () => {
     return (
-        <div style={{marginTop: '95px'}}> 
-            <h3>sdasdsadsd</h3>
-            
-            <h3>sdasdsadsd</h3>
-            <h3>sdasdsadsd</h3>
-            <h3>sdasdsadsd</h3>
-        </div>
+        <Box style={bannerBg} sx={{ flexGrow: 1 , width: 1}}>
+            <Grid container spacing={2}>
+                <Grid item xs={12} md={6} sx={{ paddingLeft: '50px' }}>
+                    <Box style={{}} >
+                        <Typography variant="h5" sx={{ width: '50%', fontWeight: '700', color: '#fff', background: '#000'}} style={{marginTop: '250px', padding: '20px', borderRadius: '0 10px 10px 0'}}>
+                            DON'T DREAM IT <br /> DRIVE IT!
+                        </Typography>
+                    </Box>
+                </Grid>
+                <Grid item xs={12} md={6} style={{}}>
+                    {/* <img style={{ width: '400px' }} src={background} alt="" /> it looks bad design*/}
+                </Grid>
+            </Grid>
+
+        </Box>
+
     );
 };
 
